c2b22b47533c70feff75fa60767f1a65ea4f071f
psych0wh1z <ikeagudike@gmail.com>
2024-09-28 18:45:00 +0100

    Remove ethers from list of dependencies

--- src/hooks/useWalletConnexion.jsx
+++ src/hooks/useWalletConnexion.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect, useCallback } from 'react';
-import { ethers } from 'ethers';
+// import { ethers } from 'ethers';
 
 const useWalletConnexion = () => {
   const [account, setAccount] = useState(null);
